5a1142e815aef53c5d49466959b748651faf62e2
henrytorres9 <hentores9@gmail.com>
2022-11-05 15:24:07 -0600

    Creando privilegios (roles): Dev y Reclutador. Se ocultó el acceso al rol: Dev, para listar las vacantes. Se ocultó el acceso al rol: Dev, para crear vacantes.

--- app/Http/Controllers/VacanteController.php
+++ app/Http/Controllers/VacanteController.php
@@ -15,6 +15,7 @@ class VacanteController extends Controller
   public function index()
   {
     //
+    $this->authorize('viewAny', Vacante::class);
     return view('vacantes.index');
   }
 
@@ -26,6 +27,7 @@ class VacanteController extends Controller
   public function create()
   {
     //
+    $this->authorize('create', Vacante::class);
     return view('vacantes.create');
   }
 
